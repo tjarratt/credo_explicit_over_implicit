8d931339b41b96f4df5b23005f5c7e3774367eee
Tim Jarratt <tim.jarratt@maersk.com>
2026-03-10 10:04:53 +0100

    Setup hex package

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule CredoExplicitOverImplicit.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/tjarratt/credo_explicit_over_implicit"
+
   def project do
     [
       app: :credo_explicit_over_implicit,
-      version: "0.1.0",
+      name: "CredoExplicitOverImplicit",
+      description:
+        "Provides Credo checks that optimize for making code explicit rather than implicit",
+      version: "1.0.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -20,6 +27,28 @@ defmodule CredoExplicitOverImplicit.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w[
+        README.*
+        lib
+        LICENSE
+        mix.exs
+      ],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github_url},
+      maintainers: ["Tim Jarratt"]
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      extras: [{:"README.md", [title: "Overview"]}],
+      source_url: @github_url
     ]
   end
 end
